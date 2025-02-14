8616a97fa242eb92bafff35a172dc6612fde7cef
FariaaA09 <fariaaahmed@rhs.school.nz>
2025-02-14 14:43:50 +1300

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,9 @@
+#01_variables
+#Use sensible descriptive variables
+#Variables are always lowercase
+#Constant names are uppercase
+#Variable names can have a mix of numbers, letters and underscores as long as the first one is a letter.
+#Constants do not change but variables do.
+
+v4ri4bles = "Pizza"
+CONSTANT = "I am hungry"
